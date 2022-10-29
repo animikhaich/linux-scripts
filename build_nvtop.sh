6ab10636993cd6b73af5b60611664a8ad99ad11b
Animikh Aich <animikhaich@gmail.com>
2022-10-29 12:06:49 -0400

    Added Dependencies: libncursesw5-dev libdrm-dev

--- build_nvtop.sh
+++ build_nvtop.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install -y git libncurses5-dev libncursesw5-dev cmake
+sudo apt update && sudo apt install -y git libncurses5-dev libncursesw5-dev libdrm-dev libudev-dev cmake
 
 git clone https://github.com/Syllo/nvtop.git
 mkdir -p nvtop/build && cd nvtop/build
